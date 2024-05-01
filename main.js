25d6e2632c03152ae4cf8aadd2ea1500569cd7cb
DmitryOverkin <dimaoverkin@mail.ru>
2024-05-01 19:39:13 +0300

    progress bar

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ window.addEventListener("scroll", function changeBgcolor() {
 
 // progressbar
 
-let circleProgress = document.querySelector(".circle-progress");
-let progressValue = document.querySelector(".progress-value");
 let skillsContainer = document.getElementById("skills");
+let progressStarted = false;
 
 let skills = [
   {
@@ -46,33 +45,62 @@ let skills = [
   },
 ];
 
-let progressStartValue = 0;
-let speed = 10;
+let speed = 40;
 
-for (let i = 0; i <= skills.length; i++) {
+for (let i = 0; i < skills.length; i++) {
+  let progressContainer = document.createElement("div");
+  progressContainer.className = "progress-container";
 
-  skillsContainer.insertAdjacentHTML(
-    "beforeend",
-    `
-        <div class="progress-container">
-            <div class="circle-progress">
-                <span class="progress-value">${skills[i].skillValue}</span>
-            </div>
-            <span class="skill-text">${skills[i].skillName}</span>
-        </div> 
-    `
-  );
+  let circleProgress = document.createElement("div");
+  circleProgress.className = "circle-progress";
+
+  let progressValue = document.createElement("span");
+  progressValue.className = "progress-value";
+  progressValue.textContent = "0%";
+
+  let skillText = document.createElement("span");
+  skillText.className = "skill-text";
+  skillText.textContent = skills[i].skillName;
+
+  progressContainer.appendChild(circleProgress);
+  progressContainer.appendChild(progressValue);
+  progressContainer.appendChild(skillText);
+
+  skillsContainer.appendChild(progressContainer);
+
+  let progressStartValue = 0;
+  let progressEndValue = skills[i].skillValue;
+
+  let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`;
+    circleProgress.style.background = `conic-gradient(#ff6333 ${progressStartValue * 3.6}deg, #928A97 0deg)`;
+
+    if (progressStartValue == progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+
+  // Проверяем текущую позицию прокрутки страницы
+  window.addEventListener("scroll", () => {
+    let scrollPosition = this.scrollY
+    if (scrollPosition > 2450 && !progressStarted) {
+      // Запускаем анимацию прогрессбара, если позиция прокрутки больше 2450 и прогрессбар еще не был запущен
+      progressStartValue = 0;
+      progressEndValue = skills[i].skillValue;
+      progressValue.textContent = "0%";
+      circleProgress.style.background = `conic-gradient(#928A97 0deg, #ff6333 0deg)`;
+      clearInterval(progress);
+      progress = setInterval(() => {
+        progressStartValue++;
+        progressValue.textContent = `${progressStartValue}%`;
+        circleProgress.style.background = `conic-gradient(#ff6333 ${progressStartValue * 3.6}deg, #928A97 0deg)`;
+
+        if (progressStartValue == progressEndValue) {
+          clearInterval(progress);
+        }
+      }, speed);
+      progressStarted = true;
+    }
+  });
 }
-
-// let progress = setInterval(() => {
-//     progressStartValue++
-//     console.log(progressStartValue)
-//     console.log(progressEndValue)
-
-//     progressValue.textContent = `${progressStartValue}%`
-// circleProgress.style.background = `conic-gradient(#ff6333 ${progressStartValue * 3.6}deg, #928A97 0deg)`
-
-//     if(progressStartValue == progressEndValue){
-//         clearInterval(progress)
-//     }
-// }, speed);
