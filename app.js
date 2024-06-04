bbf475b67802c7ef8853adfbd06c82c6755502ed
DmitryOverkin <dimaoverkin@mail.ru>
2024-06-04 13:46:05 +0300

    worket on images

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('static'));
+
 app.use("/js", express.static(`${__dirname}/js`));
 app.use("/css", express.static(`${__dirname}/css`));
-app.use("/images", express.static(__dirname + "/static/images"));
+
+
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
